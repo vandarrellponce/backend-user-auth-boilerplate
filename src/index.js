ca019e29feabcae1e468b47c354b1a2733233515
Van Darrell Ponce <vanponce91@gmail.com>
2020-10-11 14:23:18 +0800

    fixed exposed URIs

--- src/index.js
+++ src/index.js
@@ -3,37 +3,35 @@ const express = require('express')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
-
 // SET UP APP
 const app = express()
 app.use(express.json())
 
 // SET UP MIDDLEWARE FOR APP
 
-
 // SET UP ROUTER
 app.use(userRouter)
 app.use(taskRouter)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5010
 app.listen(port, () => {
-     console.log('Server is up on localhost port ' + port)
+	console.log('Server is up on localhost port ' + port)
 })
 
 const multer = require('multer')
 const uploadWare = multer({
-     dest: 'images',
-     limits: {
-          fileSize: 1000000
-     },
-     fileFilter(req, file, cb){
-          if(!file.originalname.match(/\.(doc|docx)$/)) {
-               return cb(new Error('Please upload a word document'))
-          }
-          cb(undefined, true)
-     }
+	dest: 'images',
+	limits: {
+		fileSize: 1000000,
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(doc|docx)$/)) {
+			return cb(new Error('Please upload a word document'))
+		}
+		cb(undefined, true)
+	},
 })
 
 app.post('/upload', uploadWare.single('upload'), (req, res) => {
-     res.send()
+	res.send()
 })
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,28 +1,30 @@
 // CRUD create read update delete, Inserting Document without the mongoose model
-
+const dotenv = require('dotenv')
 const mongodb = require('./mongodb')
 const MongoClient = mongodb.MongoClient
 
-const connectionURL = `mongodb+srv://darrell:admin12345@cluster0.kimqq.mongodb.net/`
+dotenv.config()
+const connectionURL = process.env.MONGO_URI
+MongoClient.connect(
+	connectionURL,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err, client) => {
+		if (err) return console.log('Unable to connect to database!')
 
-MongoClient.connect(connectionURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, (err, client) => {
+		console.log('Database connected!')
 
-    if(err) return console.log('Unable to connect to database!')
-
-    console.log('Database connected!')
-
-    const db = client.db('TaskDB')
-    // CREATE
-    // Inserting one document to TaskDB database, users collection:
-   /*  db.collection('temp-users').insertOne({
+		const db = client.db('TaskDB')
+		// CREATE
+		// Inserting one document to TaskDB database, users collection:
+		/*  db.collection('temp-users').insertOne({
         name: 'Darrell',
         aga: 29
     }) */
-    // Inserting multiple document to same db and collection:
-   /*  db.collection('users').insertMany([
+		// Inserting multiple document to same db and collection:
+		/*  db.collection('users').insertMany([
         {
             name: 'Jen',
             age: 28
@@ -34,14 +36,14 @@ MongoClient.connect(connectionURL, {
         if(err) return console.log('Unable to insert document!')
         console.log(result.ops)
     }) */
-    // READ
-    // Query one document
-  /* db.collection('users').findOne({name:'Jen'}, (err, doc) => {
+		// READ
+		// Query one document
+		/* db.collection('users').findOne({name:'Jen'}, (err, doc) => {
       if(err) return console.log(err)
       console.log(doc)
   }) */
-    // Query multiple documents (returns a cursor)
-/*   db.collection('users').find({age: 27}).toArray((err, docs) => {
+		// Query multiple documents (returns a cursor)
+		/*   db.collection('users').find({age: 27}).toArray((err, docs) => {
       if (err) return console.log(err)
       console.log(docs)
   })
@@ -50,25 +52,25 @@ MongoClient.connect(connectionURL, {
     if (err) return console.log(err)
     console.log(count)
 }) */
-    // UPDATE
-    // Updating one document using $set
-/*     db.collection('users').updateOne({name: 'Jen'}, {
+		// UPDATE
+		// Updating one document using $set
+		/*     db.collection('users').updateOne({name: 'Jen'}, {
         $set:{
             name: 'Mike'
         }
     })
     .then((result) => console.log(result))
     .catch((err) => console.log(err)) */
-    // Updating document using $inc
-   /*  db.collection('users').updateOne({name: 'Gunther'}, {
+		// Updating document using $inc
+		/*  db.collection('users').updateOne({name: 'Gunther'}, {
         $inc:{
             age: 1
         }
     })
     .then(result => console.log(result))
     .catch(err => console.log(err)) */
-    // Updating multiple document
-/*     db.collection('tasks').updateMany({completed: false}, {
+		// Updating multiple document
+		/*     db.collection('tasks').updateMany({completed: false}, {
         $set:{
             completed:true
         }
@@ -76,10 +78,11 @@ MongoClient.connect(connectionURL, {
     .then(result => console.log('updated'))
     .catch(err => console.log(err)) */
 
-    // DELETE
-    // Deleteing multiple document
-    db.collection('users').deleteMany({age:29})
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
-
-})
+		// DELETE
+		// Deleteing multiple document
+		db.collection('users')
+			.deleteMany({ age: 29 })
+			.then((result) => console.log(result))
+			.catch((err) => console.log(err))
+	}
+)
